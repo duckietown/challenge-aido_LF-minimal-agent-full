079e2bfaaeed0e3ca6a8258565d4ef5ed9440a9f
Andrea Censi <acensi@ethz.ch>
2021-10-21 13:23:11 +0200

    update of pre-commit

--- agent_full.py
+++ agent_full.py
@@ -2,6 +2,7 @@
 from aido_agents import FullAgent
 from aido_schemas import protocol_agent_DB20_fullstate, wrap_direct
 
+
 def main() -> None:
     node = FullAgent()
     protocol = protocol_agent_DB20_fullstate
